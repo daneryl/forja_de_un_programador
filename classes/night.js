673b581c7f6ae5e5f6648fc1a7772189f98f3871
daneryl <daneryl@txau-2.local>
2013-10-11 18:32:34 -0500

    integration tests started

---
+++ classes/night.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var father = require('./father.js');
+var baby = require('./baby.js');
+
+module.exports = function(cryings) {
+
+  baby.awake(cryings);
+  var actions = father.hear(baby);
+  var sleep_time = father.do(actions);
+
+  var followers_lost = 0;
+
+  if(sleep_time.minutes < 240){
+    followers_lost = 200;
+  }
+
+  return actions + ' ('+sleep_time.toString()+') ' + followers_lost + ' followers menos';
+
+};
---
+++ specs/night_spec.js
@@ -0,0 +1,65 @@
+/*jshint expr: true*/
+'use strict';
+require('should');
+
+describe('night (Integration tests)', function(){
+
+  var night = require('../classes/night.js');
+
+  describe('when baby cryes "Bua-Bua-Bua-Bua"', function(){
+    it('should report "Alimentar - Alimentar - Alimentar - Alimentar (4 horas 0 minutos) 0 followers menos"', function(){
+      var input = 'Bua-Bua-Bua-Bua';
+      var expected = 'Alimentar - Alimentar - Alimentar - Alimentar (4 horas 0 minutos) 0 followers menos';
+      night(input).should.be.exactly(expected);
+    });
+  });
+
+  describe('when baby cryes "Mua-Gua-Mua-Gua"', function(){
+    it('should report "Pañal - Chupón - Pañal - Chupón (6 horas 40 minutos) 0 followers menos"', function(){
+      var input = 'Mua-Gua-Mua-Gua';
+      var expected = 'Pañal - Chupón - Pañal - Chupón (6 horas 40 minutos) 0 followers menos';
+      night(input).should.be.exactly(expected);
+    });
+  });
+
+  describe('when baby cryes "Bua-Mua-Bua-Bua"', function(){
+    it('should report "Alimentar - Pañal Alimentar - Alimentar (4 horas 45 minutos) 0 followers menos"', function(){
+      var input = 'Bua-Mua-Bua-Bua';
+      var expected = 'Alimentar - Pañal Alimentar - Alimentar (4 horas 45 minutos) 0 followers menos';
+      night(input).should.be.exactly(expected);
+    });
+  });
+
+  describe('when baby cryes "Bua-Mua-Bua-Mua-Bua"', function(){
+    it('should report "Alimentar - Pañal Alimentar - Pañal Alimentar (4 horas 30 minutos) 0 followers menos"', function(){
+      var input = 'Bua-Mua-Bua-Mua-Bua';
+      var expected = 'Alimentar - Pañal Alimentar - Pañal Alimentar (4 horas 30 minutos) 0 followers menos';
+      night(input).should.be.exactly(expected);
+    });
+  });
+
+  describe('when baby cryes "Mua-Bua-Gua"', function(){
+    it('should report "Pañal Alimentar - Chupón (6 horas 35 minutos) 0 followers menos"', function(){
+      var input = 'Mua-Bua-Gua';
+      var expected = 'Pañal Alimentar - Chupón (6 horas 35 minutos) 0 followers menos';
+      night(input).should.be.exactly(expected);
+    });
+  });
+
+  describe('when baby cryes "Mua-Bua-Bua-Gua-Mua-Bua-Gua-Bua"', function(){
+    it('should report "Pañal Alimentar - Alimentar Chupón - Pañal Alimentar - Chupón - Alimentar (3 horas 15 minutos) 200 followers menos"', function(){
+      var input = 'Mua-Bua-Bua-Gua-Mua-Bua-Gua-Bua';
+      var expected = 'Pañal Alimentar - Alimentar Chupón - Pañal Alimentar - Chupón - Alimentar (3 horas 15 minutos) 200 followers menos';
+      night(input).should.be.exactly(expected);
+    });
+  });
+
+  describe('when baby cryes "Bua-Gua-Bua-Mua"', function(){
+    it('should report "Alimentar - Chupón Alimentar Pañal (5 horas 45 minutos) 0 followers menos"', function(){
+      var input = 'Bua-Gua-Bua-Mua';
+      var expected = 'Alimentar - Chupón Alimentar Pañal (5 horas 45 minutos) 0 followers menos';
+      night(input).should.be.exactly(expected);
+    });
+  });
+
+});
