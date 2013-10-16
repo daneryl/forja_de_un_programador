d65eff9d68f5b46cc9a14fe129046ab087983d21
daneryl <daneryl@txau-2.local>
2013-10-15 23:27:37 -0500

    refactor on father.do using new actions time() method

--- scripts/father.js
+++ scripts/father.js
@@ -1,15 +1,6 @@
 'use strict';
 
-var action_times_in_minutes = {
-  'Chupón': 10,
-  'Pañal': 30,
-  'Alimentar': 60,
-  'Pañal Alimentar': 75,
-  'Alimentar Chupón': 65,
-  'Chupón Alimentar Pañal': 75
-};
-
-var sleep_time_factory = require('./sleep_time.js');
+var sleep_time = require('./sleep_time.js');
 var actions_factory = require('./actions.js');
 
 module.exports = {
@@ -21,14 +12,7 @@ module.exports = {
   },
 
   do: function(actions) {
-    var sleep_time = sleep_time_factory.new();
-
-    var time;
-    actions.forEach(function(action) {
-      time = sleep_time.extract(action_times_in_minutes[action]);
-    });
-
-    return time;
+    return sleep_time.new().extract(actions.time());
   }
 
 };
--- specs/father_spec.js
+++ specs/father_spec.js
@@ -6,6 +6,11 @@ describe('Father', function(){
 
   var father = require('../scripts/father.js');
   var baby = require('../scripts/baby.js');
+  var actions;
+
+  beforeEach(function(){
+    actions = require('../scripts/actions.js').new();
+  });
 
   describe('when hears anything', function(){
     it('should return actions', function(){
@@ -20,49 +25,49 @@ describe('Father', function(){
 
   describe('when doing "Chupón" action', function(){
     it('should sleep 10m less', function(){
-      var actions = ['Chupón'];
+      actions.push('Chupón');
       father.do(actions).toString().should.be.exactly('7 horas 50 minutos');
     });
   });
 
   describe('when doing "Pañal" action', function(){
     it('should sleep 30m less', function(){
-      var actions = ['Pañal'];
+      actions.push('Pañal');
       father.do(actions).toString().should.be.exactly('7 horas 30 minutos');
     });
   });
 
   describe('when doing "Alimentar" action', function(){
     it('should sleep 1h less', function(){
-      var actions = ['Alimentar'];
+      actions.push('Alimentar');
       father.do(actions).toString().should.be.exactly('7 horas 0 minutos');
     });
   });
 
   describe('when doing "Pañal - Chupón - Pañal - Chupón" action', function(){
     it('should sleep 1h 20m less', function(){
-      var actions = ['Pañal', 'Chupón', 'Pañal', 'Chupón'];
+      actions.push('Pañal', 'Chupón', 'Pañal', 'Chupón');
       father.do(actions).toString().should.be.exactly('6 horas 40 minutos');
     });
   });
 
   describe('when doing "Pañal Alimentar" action', function(){
     it('should sleep 1h 15m less', function(){
-      var actions = ['Pañal Alimentar'];
+      actions.push('Pañal Alimentar');
       father.do(actions).toString().should.be.exactly('6 horas 45 minutos');
     });
   });
 
   describe('when doing "Alimentar Chupón" action', function(){
     it('should sleep 1h 5m less', function(){
-      var actions = ['Alimentar Chupón'];
+      actions.push('Alimentar Chupón');
       father.do(actions).toString().should.be.exactly('6 horas 55 minutos');
     });
   });
 
   describe('when doing "Chupón Alimentar Pañal" action', function(){
     it('should sleep 1h 5m less', function(){
-      var actions = ['Chupón Alimentar Pañal'];
+      actions.push('Chupón Alimentar Pañal');
       father.do(actions).toString().should.be.exactly('6 horas 45 minutos');
     });
   });
